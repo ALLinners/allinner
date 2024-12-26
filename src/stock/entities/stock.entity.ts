bd14454b909f59e4a30307682f22145c101afa9c
wheon <up2kid@gmail.com>
2024-12-26 09:00:19 +0900

    🔥 주석 삭제

--- src/stock/entities/stock.entity.ts
+++ src/stock/entities/stock.entity.ts
@@ -7,15 +7,6 @@ import {
 } from 'typeorm';
 import { Trader } from '../../trader/entities/trader.entity';
 
-/*
-create table stock (
-    id serial primary key,
-    reuters_code text not null,
-    name text not null,
-    traders_name text references trader(name)
-);
- */
-
 @Entity('stock')
 export class Stock {
   @PrimaryGeneratedColumn()
