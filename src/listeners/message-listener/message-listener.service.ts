8c36cbd3296428ad592552472e4816bd28e367a5
최지오, Gio Choi <techno.work@icloud.com>
2024-12-27 10:49:16 +0900

    refactoring for switch->if for message-listener.service.ts.

--- src/listeners/message-listener/message-listener.service.ts
+++ src/listeners/message-listener/message-listener.service.ts
@@ -19,79 +19,68 @@ export class MessageListenerService {
   async handleMessage(message: OmitPartialGroupDMChannel<Message<boolean>>) {
     const [command, ...args] = message.content.slice(1).split(' ');
 
-    switch (command) {
-      case '가입':
-        try {
-          const user = await this.userService.signup({
-            id: message.author.id,
-            name: message.author.globalName,
-          });
-
-          await message.reply(user.name + '님 가입을 환영합니다.');
-        } catch (e) {
-          if (e instanceof ConflictException) {
-            await message.reply(
-              message.author.globalName + '님은 이미 가입되어 있습니다.',
-            );
-          }
-        }
-
-        break;
-
-      case '탈퇴':
-        try {
-          await this.userService.delete(message.author.id);
-          await message.reply('탈퇴가 완료되었습니다.');
-        } catch (e) {
-          if (e instanceof ConflictException) {
-            await message.reply(
-              message.author.globalName + '님은 가입되지 않은 사용자입니다.',
-            );
-          }
-        }
-        break;
-
-      case '주식': // !주식 테슬라
-        const stockName = args[0]; // 테슬라
-
-        if (!stockName) {
-          message.channel.send("'!주식 <종목명>' 으로 입력해주세요");
-          break;
-        }
-        const findStock = await this.stockService.findByName(stockName);
-        console.log(JSON.stringify(findStock));
-
-        if (!findStock) {
-          message.channel.send(stockName + ' (이)라는 종목을 찾을 수 없어요.');
-          break;
-        }
-
-        const stockData = await fetchStockData(findStock);
-
-        const stockEmbed = await createStockEmbed(stockData);
-        const stockRow = await createStockButton(stockData);
-
-        await message.channel.send({
-          embeds: [stockEmbed],
-          components: [stockRow],
+    if (!command) {
+      message.channel.send('명령어를 입력해주세요.');
+    } else if (command === '가입') {
+      try {
+        const user = await this.userService.signup({
+          id: message.author.id,
+          name: message.author.globalName,
         });
 
-        if (this.messageCacheService.getState(message.author.id))
-          this.messageCacheService.deleteState(message.author.id);
-        this.messageCacheService.setState(message.author.id, message);
+        await message.reply(user.name + '님 가입을 환영합니다.');
+      } catch (e) {
+        if (e instanceof ConflictException) {
+          await message.reply(
+            message.author.globalName + '님은 이미 가입되어 있습니다.',
+          );
+        }
+      }
+    } else if (command === '탈퇴') {
+      try {
+        await this.userService.delete(message.author.id);
+        await message.reply('탈퇴가 완료되었습니다.');
+      } catch (e) {
+        if (e instanceof ConflictException) {
+          await message.reply(
+            message.author.globalName + '님은 가입되지 않은 사용자입니다.',
+          );
+        }
+      }
+    } else if (command === '주식') {
+      const stockName = args[0]; // 테슬라
 
-        break;
+      if (!stockName) {
+        message.channel.send("'!주식 <종목명>' 으로 입력해주세요");
+        return;
+      }
+      const findStock = await this.stockService.findByName(stockName);
+      console.log(JSON.stringify(findStock));
 
-      case '종목':
-        const findStocks = await this.stockService.findAll();
-        const stockList = findStocks.map((stock) => stock.name);
-        const stockTypeEmbed = createStockTypeEmbed(stockList);
-        message.channel.send({ embeds: [stockTypeEmbed] });
-        break;
+      if (!findStock) {
+        message.channel.send(stockName + ' (이)라는 종목을 찾을 수 없어요.');
+      }
 
-      default:
-        message.channel.send(command + ' (이)라는 명령어가 존재하지 않아요.');
-        break;
+      const stockData = await fetchStockData(findStock);
+
+      const stockEmbed = await createStockEmbed(stockData);
+      const stockRow = await createStockButton(stockData);
+
+      await message.channel.send({
+        embeds: [stockEmbed],
+        components: [stockRow],
+      });
+
+      if (this.messageCacheService.getState(message.author.id))
+        this.messageCacheService.deleteState(message.author.id);
+      this.messageCacheService.setState(message.author.id, message);
+    } else if (command === '종목') {
+      const findStocks = await this.stockService.findAll();
+      const stockList = findStocks.map((stock) => stock.name);
+      const stockTypeEmbed = createStockTypeEmbed(stockList);
+      message.channel.send({ embeds: [stockTypeEmbed] });
+    } else {
+      message.channel.send(command + ' (이)라는 명령어가 존재하지 않아요.');
     }
   }
 }
